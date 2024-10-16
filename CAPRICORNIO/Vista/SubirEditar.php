3b9fa12dd6d9fbfb5111ce669ea547db506a7ea6
DanielMaldonado <dannymaldonado24yt@gmail.com>
2024-10-15 19:08:13 -0500

    #7-editarTerminado

--- CAPRICORNIO/Vista/SubirEditar.php
+++ CAPRICORNIO/Vista/SubirEditar.php
@@ -1,72 +1,75 @@
 <?php
+include("../Controlador/bd.php");
+
+$estado1 = $Conexion->query("SELECT CATEGORIA, NOMBRE FROM categoria");
+$estado2 = $Conexion->query("SELECT PLATAFORMAS, NOMBRE FROM plataformas");
+
+
 echo'
 <!DOCTYPE html>
 <html>
 <head>
-     <link rel="stylesheet" href="../CSS/StyleEditar.css">
+     <link rel="stylesheet" href="../CSS/EditarSubir.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../Controlador/bd.php">
 
 </head>
-
-
-
-
-
-
-';
-?>
-
 <body>
 
     <div class="contenedor_Editar">
     <form action="../Vista/navegacionsubir.php" method="get">
     <button class="Btn_Regresar"><i class="fa-solid fa-xmark"></i></button>
-</form>
-        <form action="" method="POST">
-            <h1>Detalles</h1>
+    </form>
+        <form action="../Controlador/GuardarEditor.php" method="POST" enctype="multipart/form-data">
+            <h1 class="titulo1">Detalles</h1>
             <input type="text" placeholder="Titulo (Obligatorio)" class="titulo" name="titulo"><br>
             <input type="text" placeholder="Descripción" class="descripcion" name="descripcion">
             <h2>Portada</h2>
-            <h4 class="Text_Img">Establece una imagen para tu portada</h4>
-            <img src="../IMG/10.jpg" alt="" width="150" height="150">
+            <h4 class="Text_Img">Elige una imagen para tu pelicula</h4>
+            <h2 class="text_trailer">Trailer</h2>
+            <h4 class="Text_Vid">Elige un video para tu pelicula</h4>
             <h2 class="Generos">Genero</h2>
-            <h4 class="Text_gen">Elige el genero de tu pelicula</h4>
-            <form>
-    <select class="Genero" id="Genero">
-        <option>Acción</option>
-        <option>Aventura</option>
-        <option>Catástrofe</option>
-        <option>Ciencia ficción</option>
-        <option>Comedia</option>
-        <option>Documental</option>
-        <option>Drama</option>
-        <option>Fantasía</option>
-        <option>Terror</option>
-    </select>
-</form>
-            <h2 class="platafor">Etiquetas</h2>
+            <h4 class="Text_gen">Elige de que genero es la pelicula</h4>
+            <h2 class="platafor">plataforma</h2>
             <h4 class="Text_pla">Elige en que plataforma puedes encontrarlo</h4>
-            <form>
-    <select class="Genero" id="Etiquetas">
-        <option>Netflix</option>
-        <option>Amazon Prime Video</option>
-        <option>Disney+</option>
-        <option>HBO Max</option>
-        <option>Rakuten TV</option>
-        <option>Filmin</option>
-        <option>Movistar Plus+</option>
-        <option>Pluto TV</option>
-        <option>Batanga</option>
-
-    </select>
-</form>
-<video class="videoEditar" width="470px" height="270" controls>
-<source src="../IMG/Video1.mp4" type="">
-</video>
- 
+  
             <button class="btn_guardar">Guardar</button>
+';
+?>
+
+
+    <select name="categoria" class="Genero" id="Genero"> 
+    <?php while ($row = $estado1 ->fetch_assoc()) {?>
+        <option value="<?php echo $row['CATEGORIA'];?>"><?php echo $row['NOMBRE'];?></option>
+        <?php
+    }
+    ?>
+
+  </select>
+  <select name="etiquetas" class="Genero" id="Etiquetas"> 
+    <?php while ($row = $estado2 ->fetch_assoc()) {?>
+        <option value="<?php echo $row['PLATAFORMAS'];?>"><?php echo $row['NOMBRE'];?></option>
+        <?php
+    }
+    ?>
+  </select>
+
+  <input class="imagen" type="file" name="image" required><br>
+  <input class="video" type="file" name="media" required>
+  
+  
+  
         </form>
+        
     </div>
 </body>
+
+
+
+
+
+</html>
+
+
+
 
--- CAPRICORNIO/Vista/navegacionsubir.php
+++ CAPRICORNIO/Vista/navegacionsubir.php
@@ -1,9 +1,10 @@
 <?php
+include("../Controlador/bd.php");
 echo'
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="../CSS/Subir.css">
+    <link rel="stylesheet" href="../CSS/StyleSubir.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../Controlador/bd.php">
 
@@ -19,10 +20,43 @@ echo'
     </div>
     <form action="../Vista/SubirEditar.php" method="get">
     <button id="emergencyButton" class="Subir"><i class="fa-solid fa-plus"></i></button>
+    ';
+?>
 </form>
+   
+  <div class="ListaEditar">
+  <table >
+<?php
+  $sql="SELECT * from contenido";
+  $resultado=mysqli_query($Conexion,$sql);
+  $resultado2=$Conexion->query($sql);
+ 
+  
+  
+  
+  while($mostrar=mysqli_fetch_array($resultado) ){
+
+    ?>
+    <tr>
+    <td class="Cuadro1">
+          <video width="440" height="260" controls>
+        <source src="<?php echo $mostrar['filepath2'] ?>" type="video/mp4">
+    </video></td>
+    <td class="Cuadro2"><span class="Nombre1"><?php echo $mostrar['NOMBRE']?></span> <br> 
+                        <span class="Nombre2"><?php echo $mostrar['SINOPSIS']?></span><br>
+                        <span class="Nombre3"><?php echo $mostrar['PLATAFORMAS']?></span></td>
+    </tr>
+   
+    
+    
+    <?php
+  }
+  ?>
+  </table>
+  </div>
+
 </body>
 </html>
-';
 
-?>
+
 
