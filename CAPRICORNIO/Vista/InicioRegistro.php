9a0ee95c6e0d7f61918b4799d7755022fb248e77
DanielMaldonado <dannymaldonado24yt@gmail.com>
2024-10-22 14:45:35 -0500

    Daniel#5

--- CAPRICORNIO/Vista/InicioRegistro.php
+++ CAPRICORNIO/Vista/InicioRegistro.php
@@ -10,8 +10,8 @@ echo'
 <body>
  <h1 class="primero">Bienvenido a tu guia favorita<br>
  para streaming</h1>
- <a href="/proyectodcc/capricornio/Vista/RegistroSesion.php" class="boton1">Regístrate</a>
- <a href="/proyectodcc/capricornio/Vista/IniciarSesion.php" class="boton2">Iniciar Sesión</a>  
+ <a href="/proyectodcc/capricornio/Vista/RegistroSesion.php" class="boton1">REGÍSTRATE</a>
+ <a href="/proyectodcc/capricornio/Vista/IniciarSesion.php" class="boton2">INICIAR SESIÓN</a>  
 </body>
 </html>
 ';
